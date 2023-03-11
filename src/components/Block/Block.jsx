c8ce30a47580c31bc9fb929f81b65c3a665e3230
Obi Baratt <obibaratt@gmail.com>
2023-03-10 22:05:10 -0800

    test npm publish criteria.

---
+++ src/components/Block/Block.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+export const Block = () => {
+  return <>This is a BLOCK.</>;
+};
--- src/stories/Test.stories.jsx
+++ src/stories/Test.stories.jsx
@@ -2,9 +2,15 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 
 import { Test } from "../components/Test";
+import { Block } from "../components/Block/Block";
 
 const stories = storiesOf("App Test", module);
 
 stories.add("App", () => {
-  return <Test />;
+  return (
+    <h1>
+      <Block />
+      <Test />
+    </h1>
+  );
 });
